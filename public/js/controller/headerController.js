8f668440d2e322ec9ac77459cb4d7c7f3ee65f3b
Gustavo Y Maruyama <gustavomaruyama@hotmail.com>
2016-05-18 21:02:05 -0300

    Alteracoes em header

--- public/js/controller/headerController.js
+++ public/js/controller/headerController.js
@@ -1,7 +1,7 @@
 /**
  * Created by João Marcos BR on 18/05/2016.
  */
-angular.module("codetutor").controller("navbarController", function($scope, $location){
+angular.module("codetutor").controller("headerController", function($scope, $location){
     $scope.navegarPara = function(destino){
         $location.path("/"+destino);
     }
