5f3c8920b9c9e172a5b939f9fefbeadbb985347d
joao marcos <j.marcos.ribeiro@live.com>
2016-06-03 08:55:35 -0400

    Pequena modificacao nos atributos de Usuario e disciplina

--- models/Usuario.js
+++ models/Usuario.js
@@ -8,16 +8,16 @@ module.exports = function (sequelize, DataTypes) {
                 primaryKey: true,
                 autoIncrement: true
             },
+            nome: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
             email: {
                 type: DataTypes.STRING,
                 unique: true,
                 allowNull: false
             },
             senha: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            nome: {
                 type: DataTypes.STRING,
                 allowNull: false
             },
